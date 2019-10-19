eed99b1dbcb9a7b6cece1a3d623798ca4db0bc3e
Katy DeCorah <katydecorah@gmail.com>
2019-10-18 20:02:12 -0400

    get list of urls

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const core = require('@actions/core');
+const Sitemapper = require('sitemapper');
+const sitemap = new Sitemapper();
 
+async function getSitemapUrls(xml) {
+  return await sitemap.fetch(xml).then(sites => sites.sites.map(url => url));
+}
 
 try {
-  core.exportVariable('urls', 'https://katydecorah.com\nhttps://katydecorah.com/epicurean/rose-apple-tart/');
+  const SITEMAP = core.getInput('SITEMAP');
+  getSitemapUrls(SITEMAP)
+    .catch(err => {
+      core.setFailed(err.message);
+      process.exit(1);
+    })
+    .then(urls => {
+      core.exportVariable('urls', urls.join('\n'));
+      process.exit();
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
